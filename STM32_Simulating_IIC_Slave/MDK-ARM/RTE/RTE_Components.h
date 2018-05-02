748a7c3a433452cfd44e181026ddff2067f430fb
wuxiuhua <wuxiuhua@benewake.com>
2018-05-02 10:04:03 +0800

    Create a new project
    Create a new project by STM32CubeMX

---
+++ STM32_Simulating_IIC_Slave/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_Simulating_IIC_Slave' 
+ * Target:  'STM32_Simulating_IIC_Slave' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
